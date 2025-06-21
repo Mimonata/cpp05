b79fa3c4d82a1b558cbd919a00e2dfe6de63f817
Mimonata <sabina_pitul@yahoo.de>
2025-06-21 14:57:04 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,61 @@
 /*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 21:35:35 by spitul            #+#    #+#             */
-/*   Updated: 2025/06/20 07:30:09 by spitul           ###   ########.fr       */
+/*   Updated: 2025/06/21 14:56:17 by spitul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+#include <iostream>
+
 int	main(void)
 {
-	Bureaucrat	a("Mimo", 3);
-	Bureaucrat	b("Aga", 3);
-	Bureaucrat	c("Tini", 3);
-
+	try
+	{
+		Bureaucrat	a("Ret", 160);
+	}
+	catch(Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	b("Ret", 0);
+	}
+	catch(Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
 	
+	Bureaucrat	c("Norman", 3);
+	std::cout << ORG << c;
+	
+	try
+	{
+		c.incrementGrade();
+		std::cout << EMG << c;
+		c.incrementGrade();
+		std::cout << EMG << c;
+		c.incrementGrade();
+		std::cout << EMG << c;
+	}
+	catch(Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << ABG << e.what() << std::endl;
+	}
 	
 	return 0;
 }
